6f0ea4091a858360ac4464bccb0cf70aa78cf398
baglaaarav <aaravispossible19@gmail.com>
2024-02-26 15:49:14 +0530

    video 43

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const mongoUrl = "mongodb://localhost:27017/"
+
+
+
+async function connectToMongo() {
+    try {
+      await mongoose.connect(mongoUrl);
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+
+module.exports = connectToMongo;
