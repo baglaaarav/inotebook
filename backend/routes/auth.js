0a66ac42762842125d94e39a9c75fc87200248ed
baglaaarav <aaravispossible19@gmail.com>
2024-02-27 11:25:04 +0530

    video 46

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 const router = express.Router();
 //create a user usign POST , Don't reqire a auth
 
-router.get('/', (req, res) =>{
+router.post('/', (req, res) =>{
     console.log(req.body)
     const user = User(req.body)
     user.save()
