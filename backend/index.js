717058ccff93f789aec0afe0db8dca3d246bc127
baglaaarav <aaravispossible19@gmail.com>
2024-04-02 15:34:56 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 const port = 4000
 app.use(express.json())
 
- app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+ app.get("/myapi", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
 
 
